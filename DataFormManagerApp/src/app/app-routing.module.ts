1897d07704fabe15e9a959e03c25bb5bf4f759ea
Rahul-Rj96 <RahulR@rckr.com>
2019-12-06 12:29:10 +0530

    angular forms has updated

--- DataFormManagerApp/src/app/app-routing.module.ts
+++ DataFormManagerApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { UserspecificformComponent } from './userspecificform/userspecificform.component';
 
 
 const routes: Routes = [
   { path: 'login' , component: LoginComponent},
+  { path: 'userspecificform' ,component: UserspecificformComponent},
   { path: 'dashboard' , component: DashboardComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 
@@ -16,3 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- DataFormManagerApp/src/app/app.module.ts
+++ DataFormManagerApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,9 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { UserspecificformComponent } from './userspecificform/userspecificform.component';
+import { FormdetailsComponent } from './formdetails/formdetails.component';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +18,12 @@ import { HeaderComponent } from './header/header.component';
     LoginComponent,
     DashboardComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserspecificformComponent,
+    FormdetailsComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
