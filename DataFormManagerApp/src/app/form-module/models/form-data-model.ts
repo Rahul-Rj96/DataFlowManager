5e6f74d2682d0b98776fd03b3db90f61115bfc0d
Rahul-Rj96 <RahulR@rckr.com>
2020-01-09 14:43:57 +0530

    FormName added.

--- DataFormManagerApp/src/app/form-module/models/form-data-model.ts
+++ DataFormManagerApp/src/app/form-module/models/form-data-model.ts
@@ -4,11 +4,13 @@ import { Datesmodel } from './datesmodel';
 export class FormDataModel {
     FormId: number;
     FormType: string;
+    FormName: string;
     FormData: Array<DataValueModel>;
     EffectiveDate: Datesmodel;
-    constructor(formType: string, formData: Array<DataValueModel>, effectiveDate: Datesmodel, formId: number = null) {
+    constructor(formType: string, formName: string, formData: Array<DataValueModel>, effectiveDate: Datesmodel, formId: number = null) {
         this.FormId = formId;
         this.FormType = formType;
+        this.FormName = formName;
         this.FormData = formData;
         this.EffectiveDate = effectiveDate;
     }
